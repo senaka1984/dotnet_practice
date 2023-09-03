0989ad42e8414afa7a659094a615a455ef2a39a8
senaka1984 <senakaelladeniya@gmail.com>
2023-09-03 12:56:16 +1200

    rollback product class

--- Practice_Api/Practice_Api/Models/Product.cs
+++ Practice_Api/Practice_Api/Models/Product.cs
@@ -2,18 +2,8 @@
 {
     public class Product
     {
-       IEnumerable<ProductItem> ProductItems { get; set; }
-
-    }
-
-    public class ProductItem
-    {
-        public int Id { get; set; } = 0;    
-        public string Name { get; set; }    
+        public int Id { get; set; }
+        public string Name { get; set; }
         public decimal Price { get; set; }
-        public string Description { get; set; }
-
-
-
     }
 }
