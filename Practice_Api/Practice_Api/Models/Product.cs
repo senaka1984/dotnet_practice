c7aa0845359affc4f8c87b92e4ef31bd2e4c896c
senaka1984 <senakaelladeniya@gmail.com>
2023-09-03 14:40:38 +1200

    Update Product.cs

--- Practice_Api/Practice_Api/Models/Product.cs
+++ Practice_Api/Practice_Api/Models/Product.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+
+        public string Description { get; set; }
     }
 }
